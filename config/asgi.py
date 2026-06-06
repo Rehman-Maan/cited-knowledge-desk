a8f62e9d4997d37ca28ef28981fbe72cecfccb50
Rehman-Maan <129269381+Rehman-Maan@users.noreply.github.com>
2026-06-06 11:37:46 +0500

    Load Django ASGI app before chat routing

--- config/asgi.py
+++ config/asgi.py
@@ -6,11 +6,11 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-from apps.chat.routing import websocket_urlpatterns
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 django_asgi_app = get_asgi_application()
+
+from apps.chat.routing import websocket_urlpatterns  # noqa: E402
 
 application = ProtocolTypeRouter(
     {
